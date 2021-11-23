b2c26c9811f413d95aac2c5cc74ab87752d49442
AnirbanAnu2000 <anirbananu2000@gmail.com>
2021-11-23 20:08:36 +0600

    second

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA6zQWqVmmrHu7QM31F6bcJ_-x9wrr75Qo",
-  authDomain: "decent-healthcare.firebaseapp.com",
-  projectId: "decent-healthcare",
-  storageBucket: "decent-healthcare.appspot.com",
-  messagingSenderId: "335713883844",
-  appId: "1:335713883844:web:7f531c8e56f42c008f5b91"
+  apiKey: "AIzaSyAPlOZimHfc_a4pFxQnBt5d35-Qsa-wETU",
+  authDomain: "decent-healthcare2.firebaseapp.com",
+  projectId: "decent-healthcare2",
+  storageBucket: "decent-healthcare2.appspot.com",
+  messagingSenderId: "1076809773229",
+  appId: "1:1076809773229:web:7d7452969991c65e645624"
 };
 
 export default firebaseConfig;
